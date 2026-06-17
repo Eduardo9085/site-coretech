bd6f51d23376e0e647a749f0ff7fa1006b3e850e
Eduardo9085 <eduardo.9085@colegioprogresso.g12.br>
2026-06-17 15:13:14 -0300

    Delete loja directory

--- loja/includes/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-
-<footer style="text-align:center; padding: 30px; color:#666;">
-    <p>CoreTech Fullstack em PHP + MySQL</p>
-</footer>
-
-</body>
-</html>
--- loja/teste.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1>PHP funcionando normal</h1>";
-echo "<p>Se você está vendo isso, o servidor está certo.</p>";
-?>
